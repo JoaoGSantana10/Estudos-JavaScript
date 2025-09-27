34ffb109975a668205f4ae3b91ee509e32051d51
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2025-09-27 18:06:13 -0300

    Create ambiente.js

---
+++ Aula14/ambiente.js
@@ -0,0 +1,29 @@
+/*  Estruturas de repetição da aula passada 
+    var c = 1
+    while (c <= 10){
+        console.log(`passo#{c}`)
+        c++
+    }
+    Testa a condição no início e executa o bloco no final
+    Estrutura de repetição com teste lógico no final,
+*/
+
+/*
+    var c = 1
+    do{
+        console.log(`passo ${c}`)
+        c++
+    } while (c <=10)
+     
+    Executa o bloco no início e testa a condição no final
+    Estrtura de respetição com teste lógico no início
+*/
+
+//--------------------------------
+
+// Estrutura de repetição com variável de controle
+console.log('Vai começar...')
+for(var c =1 ; c <= 10; c++){
+    console.log(`passo ${c}`)
+}
+console.log('FIM!')
