22176726e2a6145470fe52ff69ac5e6bb99c1c0e
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2025-10-02 21:24:00 -0300

    Update script.js
    JS do ex017

--- Aula14ex/ex017/script.js
+++ Aula14ex/ex017/script.js
@@ -1,19 +1,20 @@
-function Tabuada() {
-    var num = document.getElementById('txtn')
-    var tab = document.getElementById('seltab')
-    if (num.Value.lenght == 0 ){
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.length == 0 ){
         window.alert('Por favor, digite um número!')
-    }
-    else {
-        var n = Number(num.value)
-        var c = 1
+    } else {
+        let n = Number(num.value)
+        let c = 1
         tab.innerHTML = ''
         while (c <= 10) {
-            var item = document.createElement('option')
-            item.text(`${n} x ${c} = ${n*c}`)
-            item.value = `tab${c}`
-            tab.appendChild(item)
-            c++
+          let item = document.createElement('option')
+          item.text = `${n} x ${c} = ${n*c}`
+          item.value = `tab${c}`
+          tab.appendChild(item)
+          c++
         }
     }
+
 }
