649687ca79517381a976ff5b01673610b10549a8
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2025-10-07 15:07:44 -0300

    Update vetornatela.js
    forma mais simples.

--- Aula15/vetornatela.js
+++ Aula15/vetornatela.js
@@ -1,6 +1,11 @@
 let num = [5, 8, 2, 9, 3]
 num.push(7)
 num.sort()
-for(let pos=0; pos<=num.length; pos ++){
+/*for(let pos = 0; pos <= num.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+*/
+
+for(let pos in num){
     console.log(`A posição ${pos} tem o valor ${num[pos]}`)
 }
