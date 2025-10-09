40d4829229b39931fde4f15f4b6fcaa1a26798d4
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2025-10-09 14:34:44 -0300

    Update script.js

--- Aula16ex/ex018/script.js
+++ Aula16ex/ex018/script.js
@@ -22,7 +22,14 @@ function inLista(n,l) {
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
         window.alert('Tudo Ok!')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `valor ${num.value} adicionado.`
+        lista.appendChild(item)
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
+    num.value = ''
+    num.focus()
 }
+
