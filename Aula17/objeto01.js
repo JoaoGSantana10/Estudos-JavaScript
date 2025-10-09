636b9981e430bc1bfddb31ec8e1f9215469f0a50
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2025-10-09 19:34:18 -0300

    Update objeto01.js

--- Aula17/objeto01.js
+++ Aula17/objeto01.js
@@ -6,4 +6,5 @@ engordar(p = 0){
     this.peso += p
 }}    
 amigo.engordar(2)
-console.log(`${amigo.nome} pesa ${amigo.peso}kg`)            
+console.log(`${amigo.nome} pesa ${amigo.peso}kg`)
+            
